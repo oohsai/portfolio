ea17758dca1d718583cc21807bdb6914dd45c65b
oohsai <s72454726@gmail.com>
2024-04-20 03:20:09 +0530

    fixed import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import Techstack from "./_components/TechStack";
-import { Main } from "./_components/Main";
 import About from "./_components/About";
 import { Background } from "./_components/Background";
 import { Projects } from "./_components/Projects";
